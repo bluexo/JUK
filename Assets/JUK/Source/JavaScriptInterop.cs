abc552d44ac0931f9e164dcab9b1ce59329d9bda
Alvin <bluexo@hotmail.com>
2018-12-07 19:30:49 +0800

    MessagePack test

--- Assets/JUK/Source/JavaScriptInterop.cs
+++ Assets/JUK/Source/JavaScriptInterop.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using Jint;
+using Jint.Native;
 using Jint.Runtime;
+using MessagePack;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
 
 namespace JUK
 {
@@ -17,10 +21,12 @@ namespace JUK
             option.AllowClr(typeof(Application).Assembly,
                 typeof(Button).Assembly,
                 typeof(Event).Assembly,
-                typeof(PointerEventData).Assembly);
+                typeof(PointerEventData).Assembly,
+                typeof(Stopwatch).Assembly);
             option.Strict();
-            option.DebugMode();
-            option.AllowDebuggerStatement();
+            option.DiscardGlobal();
+            //option.DebugMode();
+            //option.AllowDebuggerStatement();
         });
 
         // Use this for initialization
@@ -35,13 +41,16 @@ namespace JUK
                     var txt = File.ReadAllText(files[i]);
                     JsEngine.Execute(txt);
                 }
+#else
+
 #endif
             }
             catch (JavaScriptException exc)
             {
                 Debug.LogError($"{exc},\n" +
-                    $"LineNumber={exc.LineNumber} ,\n" +
-                    $"Location={exc.Location.Source} \n");
+                    $"{nameof(exc.LineNumber)} = {exc.LineNumber}, \n" +
+                    $"{nameof(exc.Column)} = {exc.Column}), \n" +
+                    $"{nameof(exc.CallStack)} = {exc.CallStack} \n");
             }
         }
 
@@ -54,8 +63,7 @@ namespace JUK
         {
             var code = Resources.Load<TextAsset>("game");
             JsEngine.SetValue("Log", new Action<string>(Debug.Log));
-            JsEngine.SetClrValue(nameof(Startup), this);
-            JsEngine.SetClrValue(nameof(Logger), Debug.unityLogger);
+            JsEngine.SetClrValue("Startup", this);
             try
             {
                 JsEngine.Execute(code.text);
@@ -63,14 +71,64 @@ namespace JUK
             catch (JavaScriptException exc)
             {
                 Debug.LogError($"{exc},\n" +
-                    $"LineNumber={exc.LineNumber} ,\n" +
-                    $"Location={exc.Location.Source} \n");
+                    $"{nameof(exc.LineNumber)} = {exc.LineNumber}, \n" +
+                    $"{nameof(exc.Column)} = {exc.Column}), \n" +
+                    $"{nameof(exc.CallStack)} = {exc.CallStack} \n");
             }
         }
 
-        public void CallByJs()
+        [MessagePackObject]
+        public class Nested
         {
-            Debug.Log(nameof(CallByJs));
+            [Key(0)]
+            public int a { get; set; }
+            [Key(1)]
+            public string b { get; set; }
+            [Key(2)]
+            public bool c { get; set; }
+        }
+
+        [MessagePackObject]
+        public class SampleData
+        {
+            [Key(0)]
+            public int number { get; set; }
+            [Key(1)]
+            public double number2 { get; set; }
+            [Key(2)]
+            public string text { get; set; }
+            [Key(3)]
+            public bool flag { get; set; }
+            [Key(4)]
+            public List<int> list { get; set; }
+            [Key(5)]
+            public Nested obj { get; set; }
+        }
+
+        public void DeserializeByJs(byte[] buffer)
+        {
+            var obj = MessagePackSerializer.Deserialize<SampleData>(buffer);
+            Debug.Log($"{obj.text},{obj.flag},{obj.list.Count}");
+            var sw = new Stopwatch();
+            sw.Reset();
+            sw.Start();
+            for (var i = 0; i < 10000; i++)
+            {
+                var newBuffer = MessagePackSerializer.Deserialize<SampleData>(buffer);
+            }
+            sw.Stop();
+            Debug.Log("-----CS");
+            Debug.Log($"{sw.ElapsedMilliseconds}");
+            Debug.Log("CS-----");
+
+            var test = JsEngine.GetValue("test");
+            sw.Reset();
+            sw.Start();
+            Debug.Log("-----JS");
+            test.Invoke();
+            Debug.Log(sw.ElapsedMilliseconds);
+            Debug.Log("JS-----");
+            sw.Stop();
         }
 
 
